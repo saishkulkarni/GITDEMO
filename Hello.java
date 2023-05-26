076028609818147e55a688599e7b3266faaf4265
rahulmernstack <rahulmernstack@gmail.com>
2023-05-26 10:04:04 +0530

    added

---
+++ Hello.java
@@ -0,0 +1,18 @@
+class Hello{
+    public static void main(String[] args) {
+        int a=6;
+        System.out.println(a);
+
+        int b=020;
+        System.out.println(b);
+
+        int c=0b111;
+        System.out.println(c);
+
+        int d=0xAB112;
+        System.out.println(d);
+
+        int e=10_000;
+        System.out.println(e);
+    }
+}
